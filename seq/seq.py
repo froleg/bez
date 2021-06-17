096a255968ae0d3d116534073d1f79ba782f1776
froleg <frolgx@gmail.com>
2021-06-17 23:38:53 +0300

    topt

--- seq/seq.py
+++ seq/seq.py
@@ -17,7 +17,6 @@ def bz_2d_coef (d,P,t):
         J.append(special.binom(d-2,i)*(t**i)*(1-t)**(d-2-i)*(P[i+2]-2*P[i+1]+P[i]))
     return np.sum(np.array(J,dtype=np.float64))*d*(d-1)
 def curvt(d,P,T):
-    
 #   bp=np.array([[bz_coef(d,P[0],x) for x in T],[bz_coef(d,P[1],x) for x in T]])
    b1dp=np.array([[bz_1d_coef(d,P[0],x) for x in T],[bz_1d_coef(d,P[1],x) for x in T]])
    b2dp=np.array([[bz_2d_coef(d,P[0],x) for x in T],[bz_2d_coef(d,P[1],x) for x in T]])
@@ -40,4 +39,19 @@ intf=np.flip(np.append(reverse,0.0))
 insm=np.array([np.sum(intf[:x]) for x in range(1,N+1)],dtype=np.float64)
 print(intf)
 print(insm)
-print(dt*(cur[0]+cur[1])/2)
+er=0.001
+InMx=(insm[len(insm)-1])
+m=np.int(np.ceil(InMx*1/np.sqrt(8*er))+1)
+print(m)
+IntDist=np.linspace(0.0,InMx,m)
+print(IntDist[m-2])
+print(np.searchsorted(insm,IntDist[m-2]))
+tOp=[In]
+for i in range(1,m-1):
+    inUp=np.searchsorted(insm,IntDist[i])
+    el=((IntDist[i]-insm[inUp-1])/(insm[inUp]-insm[inUp-1]))*dt+tpt[inUp-1]
+    tOp.append(el)
+tOp.append(End)
+topt=np.array(tOp,dtype=np.float64)
+print(topt)
+print(insm[len(insm)-1])
